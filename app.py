1f7afa6f882b75bba8d5d945b4f3c80c38a8df2f
hitesh <hitesh.yerekar@gmail.com>
2024-07-04 15:36:56 +0530

    data transformation file added.

--- app.py
+++ app.py
@@ -4,14 +4,17 @@ from src.HY_MLPROJECT.exception import CustomException
 from src.HY_MLPROJECT.logger import logging
 from src.HY_MLPROJECT.components.data_ingestion import DataIngestion
 from src.HY_MLPROJECT.components.data_ingestion import DataIngestionConfig
+from src.HY_MLPROJECT.components.data_transformation import DataTransformation,DataTransformationConfig
 
 if __name__=="__main__":
     logging.info("Execution has started.")
-
     try:
         data_ingestion=DataIngestion()
         #data_ingestion_config=DataIngestionConfig()
-        data_ingestion.initiate_data_ingestion()
+        train_data_path,test_data_path=data_ingestion.initiate_data_ingestion()
+
+        data_transformation=DataTransformation()
+        data_transformation.initiate_data_transformation(train_data_path,test_data_path)
 
     except Exception as e:
         raise CustomException(e,sys)
